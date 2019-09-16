261f1eb2f18390635897b10d3b491214b9d409ed
ifredom <1950735817@qq.com>
2019-09-16 12:18:52 +0800

    intial

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# create-flutter-app
-使用flutter制作得一个app
+# hello
+
+A new Flutter project.
+
+## Getting Started
+
+1. material 组件字体大小为 14px.
+2. 对象上得属性，如果是动态添加得，不能使用点访问，智能使用方括号。example:
+   `tabData[i]['icon']`正确，`tabData[i].icon`错误，
---
+++ lib/pages/home.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hello/pages/activity/activity_page.dart';
+import 'package:hello/pages/first/first_page.dart';
+import 'package:hello/pages/mine/mine_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  List<Widget> _list = List();
+  List<BottomNavigationBarItem> _bottomTabs = [];
+  List tabData = [
+    {'title': Text('首页'), 'icon': Icon(Icons.home)},
+    {'title': Text('活动'), 'icon': Icon(Icons.nature)},
+    {'title': Text('我的'), 'icon': Icon(Icons.mail_outline)},
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    print('widget.userInfo');
+
+    _list..add(FirstPage())..add(ActivityPage())..add(MinePage());
+    for (var i = 0; i < tabData.length; i++) {
+      _bottomTabs.add(BottomNavigationBarItem(
+        icon: tabData[i]['icon'],
+        title: tabData[i]['title'],
+      ));
+    }
+    print(_bottomTabs.length);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('change');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // return Text('test');
+    return new Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            '首页',
+            style: TextStyle(
+              fontSize: 18.0,
+            ),
+          ),
+        ),
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _list,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: _bottomTabs,
+        currentIndex: _currentIndex,
+        onTap: _itemTapped,
+        fixedColor: Theme.of(context).primaryColor,
+      ),
+    );
+  }
+
+  void _itemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
+
+class GradientAppBar extends StatelessWidget {
+  final String title;
+  final double barHeight = 15.0;
+
+  GradientAppBar(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    return Container(
+      padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+      height: statusBarHeight + barHeight,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(
+            Icons.people,
+            color: Colors.black54,
+          ),
+          Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 18.0,
+            ),
+          ),
+          Icon(
+            Icons.search,
+            color: Colors.deepPurple,
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color(0xFF3366FF),
+            const Color(0xFF00CCFF),
+          ],
+          begin: const FractionalOffset(0.0, 1.0),
+          end: const FractionalOffset(1.0, 0.0),
+          stops: [0.0, 1.0],
+          tileMode: TileMode.clamp,
+        ),
+      ),
+    );
+  }
+}
