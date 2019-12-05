5515bc2402bdefbdab11f0eb31afca623627360d
ifredom <1950735817@qq.com>
2019-12-05 15:38:19 +0800

    flutter- android工程

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\software\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter_projects\first_app\myself"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_projects\first_app\hello"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
